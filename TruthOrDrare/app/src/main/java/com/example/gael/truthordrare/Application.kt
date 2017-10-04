ba940573e7174628fe9b2a0c83614cea81e7aaed
Malcom <gaeltshilombo@gmail.com>
2017-10-04 14:34:31 +0200

    add the base

---
+++ TruthOrDrare/app/src/main/java/com/example/gael/truthordrare/Application.kt
@@ -0,0 +1,11 @@
+package com.example.gael.truthordrare
+
+import android.app.Application
+
+/**
+ * Created on 04.10.17.
+ */
+
+class Application : Application() {
+
+}
---
+++ TruthOrDrare/app/src/main/java/com/example/gael/truthordrare/BaseView.kt
@@ -0,0 +1,10 @@
+package com.example.gael.truthordrare
+
+/**
+ * Created on 04.10.17.
+ */
+
+interface BaseView<T> {
+
+    fun setPresenter(presenter : T)
+}
